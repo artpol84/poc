b2bc24195e63aaab2f1c13bff42690422ffe9ab8
Artem Polyakov <artpol84@gmail.com>
2019-01-22 04:30:32 -0800

    arch/CPU buffers: WCB + SFENCE (sfence after every store)

---
+++ arch/cpu_buffers_analysis/wcb_sfence_n/README.md
@@ -0,0 +1,9 @@
+This is a CPU "Write Combine Buffers (WCB) + SFENCE" analysis code that tries to estimate
+the effect of sfence amount of WCB's using the following approach:
+
+1. A variative number of NT-stores are performed sequentially
+1. An SFENCE instruction is inserted in front of the last NT op to create a store fence between them.
+1. They are followed by a set of NO-OPs
+
+The base idea of the test is the following:
+* Derived from WCB test
---
+++ arch/cpu_buffers_analysis/wcb_sfence_n/generate.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+import sys
+
+tmpl_fname = sys.argv[1]
+out_fname = sys.argv[2]
+nstores = int(sys.argv[3])
+nnoops = int(sys.argv[4])
+
+def write_mov(out, offs) :
+	line = "            "
+	line += "\"vmovntdq %%ymm0, " + str(offs * 64) + "(%[array])\\n\"\n"
+	out.write(line)
+
+
+def write_asm_body(out) :
+	array_size = nstores
+	if nstores < 16:
+		array_size = 16
+	out.write("        uint64_t movq[4];\n")
+	out.write("        __m256i avx_in, avx_out[" + str(2 * array_size) + "];\n")
+	out.write("        // Make sure that we are moving with 64B steps to write to a new cache line each time\n")
+	out.write("        asm volatile (\n");
+	i = 0
+	while (i < nstores):
+		write_mov(out, i)
+		out.write("            \"sfence\\n\"\n")
+		i += 1
+
+	i = 0
+	while (i < nnoops) :
+		line = "            "
+		line += "\"nop\\n\"\n"
+		out.write(line)
+		i += 1
+    
+	out.write("            : // No output\n")
+	out.write("            : [array] \"r\" (avx_out), [in] \"r\" (&avx_in), [mvqptr] \"r\" (movq)\n");
+	out.write("            : \"memory\", \"%xmm0\");\n")
+
+def write_noops(out) :
+	out.write("        asm volatile (\n");
+
+	i = 0
+	while (i < 10000) :
+		line = "            "
+		line += "\"nop\\n\"\n"
+		out.write(line)
+		i += 1
+    
+	out.write("            : // No output\n")
+	out.write("            : \n");
+	out.write("            : \"memory\", \"%xmm0\");\n")
+
+
+
+with open(tmpl_fname, 'r') as f:
+	tmpl = f.readlines()
+
+out = open(out_fname, 'w');
+
+for l in tmpl:
+	if "asm_code" in l:
+		write_asm_body(out)
+	elif "noop_code" in l:
+		write_noops(out)
+	elif "count_init" in l:
+		out.write("    int store_cnt = " + str(nstores) + ";\n")
+	else:
+		out.write(l)
