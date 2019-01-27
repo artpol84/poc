9adee174bb04c05582103947b014177aa47865c0
Artem Polyakov <artpol84@gmail.com>
2019-01-27 10:32:00 -0800

    arch/CPU buffers: Fix test 04: ensure that 64B full writes are performed

--- arch/cpu_buffers/04_wcb_sfence_cached/generate.py
+++ arch/cpu_buffers/04_wcb_sfence_cached/generate.py
@@ -11,13 +11,17 @@ import litmus_test_lib as lib
 class wcb_w_sfence(lib.litmus_test) :
 	def test_body(self) :
 		self.out.write("        uint64_t movq[" + str(self.nstores) + "];\n")
-		self.out.write("        __m256i avx_in, avx_out[1];\n")
+		self.out.write("        __m256i avx_in, avx_out_base[3], *avx_out = avx_out_base;\n")
+		self.out.write("        if( ((uint64_t)avx_out) % 64 ) { avx_out++; }\n")
 		self.out.write("        // Make sure that we are moving with 64B steps to write to a new cache line each time\n")
 		self.out.write("        asm volatile (\n");
 
 	    # Put a long latency Non-Temporal (NT) operation
 		line = "            "
 		line += "\"vmovntdq %%ymm0, 0(%[array])\\n\"\n"
+		self.out.write(line)
+		line = "            "
+		line += "\"vmovntdq %%ymm0, 32(%[array])\\n\"\n"
 		self.out.write(line)
 
 	    # Put an SFENCE
