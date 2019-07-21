b177bb7952f005894760f3b730b7f7b9f2a22583
Artem Polyakov <artemp@mellanox.com>
2019-07-20 18:22:41 -0700

    SWIFT is running with Intel MPI

--- swift/run_impi.sh
+++ swift/run_impi.sh
@@ -1,47 +1,37 @@
 #!/bin/bash
 
-get_nproc()
-{
-    local HOSTS=$1
-    hname=`echo $HOSTS | tr ',' '\n' | head -n 1`
-    np=`ssh $hname nproc`
-    echo $np
-}
-
-get_nnodes()
-{
-    local HOSTS=$1
-    hcnt=`echo $HOSTS | tr ',' '\n' | wc -l`
-    echo "$hcnt"
-}
-
-
-ppn=$1
-if [ -z "$ppn" ]; then
-    echo "Need PPN input parameter!"
-    exit 1
+SCRIPT_PATH=$0
+if [ -L "$SCRIPT_PATH" ]; then
+    SCRIPT_PATH=`readlink $SCRIPT_PATH`
 fi
-shift 
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
 
-debug=$1
-if [ "$debug" = "debug" ]; then
-    echo "DEBUG MODE"
-    shift
+if [ -f "swift_env.sh" ]; then
+    # If user defined a local env - use it
+    . swift_env.sh
 else
-    debug=""
+    # Use the default env file
+    . $SCRIPT_DIR/swift_env.sh
 fi
 
-user_threads=""
-if [ -n "$1" ]; then
-    user_threads=$1
-    shift
-fi
+. $SCRIPT_DIR/run_common.sh
 
 module load intel/ics-18.0.4
 
-export LD_LIBRARY_PATH="/labhome/artemp/SWIFT/2019_07_09_Profiling_IMPI/build/lib/:$LD_LIBRARY_PATH"
 
-HOSTS=clx-hercules-002,clx-hercules-009,clx-hercules-010,clx-hercules-014
+if [ -z "$SWIFT_INST_DIR" ]; then
+    echo "No SWIFT_INST_DIR env found. You may need to check your swift_env.sh file if run is failing"
+else
+    export LD_LIBRARY_PATH="$SWIFT_INST_DIR/build/lib/:$LD_LIBRARY_PATH"
+fi
+
+if [ -z "$SWIFT_HOSTS" ]; then
+    echo "No SWIFT_HOSTS env found. You may need to check your swift_env.sh file if run is failing"
+fi
+
+if [ ! -f dat.conf ]; then
+    echo "No local dat.conf is found. Consider creating one from /etc/dat.conf if your interface is not there"
+fi
 
 np=`get_nproc $HOSTS`
 echo $np
@@ -49,40 +39,43 @@ echo $np
 nodes=`get_nnodes $HOSTS`
 echo $nodes
 
-echo "ppn=$ppn"
-
 threads=$(( $np / $ppn ))
 if [ -n "$user_threads" ]; then
     threads=$user_threads
 fi
 
-echo "Running on $nodes nodes, with $ppn PPN, using $threads threads"
+OUT_FILE="impi_$OUT_FILE"
 
-if [ $debug ]; then
-    mpirun -np $(($nodes * $ppn)) -genv I_MPI_DAT_LIBRARY /usr/lib64/libdat2.so \
-           -DAPL \
-            -genv I_MPI_FABRICS shm:dapl \
-            -genv I_MPI_DAPL_PROVIDER ofa-v2-mlx5_2-1u \
-            -genv DAT_OVERRIDE ./dat.conf \
-            -genv I_MPI_PIN on \
-            -genv I_MPI_PIN on \
-            -genv I_MPI_PERHOST $ppn \
-            -genv I_MPI_DEBUG 4 \
-            -hosts $HOSTS \
-            ./hello_c
-else
-    set -x
-    ulimit -s unlimited
-    mpirun -np $(($nodes * $ppn)) -genv I_MPI_DAT_LIBRARY /usr/lib64/libdat2.so \
-           -DAPL \
-            -genv I_MPI_FABRICS shm:dapl \
-            -genv I_MPI_DAPL_PROVIDER ofa-v2-mlx5_2-1u \
-            -genv DAT_OVERRIDE ./dat.conf \
-            -genv I_MPI_PIN on \
-            -genv I_MPI_PIN on \
-            -genv I_MPI_PERHOST $ppn \
-            -genv I_MPI_DEBUG 4 \
-            -hosts $HOSTS \
-            ../../swift_mpi \
-                     --cosmology --hydro --self-gravity --stars --threads=$threads -n 1024 --cooling eagle_25.yml 2>&1 | tee swift_${nodes}Nx${ppn}PPNx${threads}THR.log
-fi
+rm -f $OUT_FILE
+
+echo "Running on $nodes nodes, with $ppn PPN, using $threads threads" |& tee -a $OUT_FILE
+
+echo "mpirun -np $(($nodes * $ppn)) -genv I_MPI_DAT_LIBRARY /usr/lib64/libdat2.so \
+       -DAPL \
+        -genv I_MPI_FABRICS shm:dapl \
+        -genv I_MPI_DAPL_PROVIDER ofa-v2-mlx5_2-1u \
+        -genv DAT_OVERRIDE ./dat.conf \
+        -genv I_MPI_PIN on \
+        -genv I_MPI_PERHOST $ppn \
+        -genv I_MPI_DEBUG 4 \
+        -hosts $SWIFT_HOSTS \
+        ./swift_mpi \
+                 --cosmology --hydro --self-gravity --stars --threads=$threads -n 2048 --cooling $infile 2>&1 |& tee swift_e50_${nodes}Nx${ppn}PPNx${threads}THR.log" \
+        |& tee -a $OUT_FILE
+
+
+set -x
+ulimit -s unlimited
+mpirun -np $(($nodes * $ppn)) -genv I_MPI_DAT_LIBRARY /usr/lib64/libdat2.so \
+       -DAPL \
+        -genv I_MPI_FABRICS shm:dapl \
+        -genv I_MPI_DAPL_PROVIDER ofa-v2-mlx5_2-1u \
+        -genv DAT_OVERRIDE ./dat.conf \
+        -genv I_MPI_PIN on \
+        -genv I_MPI_PERHOST $ppn \
+        -genv I_MPI_DEBUG 4 \
+        -hosts $SWIFT_HOSTS \
+        ./swift_mpi \
+                 --cosmology --hydro --self-gravity --stars --threads=$threads -n 2048 --cooling $infile 2>&1 |& tee -a $OUT_FILE
+
+rm -Rf restart 
