11558daceebc393c2b0ca94b440e1c28eac0f89e
Artem Polyakov <artpol84@gmail.com>
2019-08-28 17:32:04 -0700

    Introduce the many-to-one pattern

--- MPI/Recv_msgrate/osu_mbw_mr.c
+++ MPI/Recv_msgrate/osu_mbw_mr.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 {
     char *s_buf, *r_buf;
     int numprocs, rank;
-    int c, curr_size;
+    int curr_size;
 
     loop_override = 0;
     skip_override = 0;
@@ -41,8 +41,6 @@ int main(int argc, char *argv[])
 
     MPI_CHECK(MPI_Comm_size(MPI_COMM_WORLD, &numprocs));
     MPI_CHECK(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
-
-    options.pairs            = numprocs / 2;
 
     int po_ret = process_options(argc, argv);
 
@@ -59,14 +57,6 @@ int main(int argc, char *argv[])
 
     if (0 == rank) {
         switch (po_ret) {
-            case PO_CUDA_NOT_AVAIL:
-                fprintf(stderr, "CUDA support not enabled.  Please recompile "
-                        "benchmark with CUDA support.\n");
-                break;
-            case PO_OPENACC_NOT_AVAIL:
-                fprintf(stderr, "OPENACC support not enabled.  Please "
-                        "recompile benchmark with OPENACC support.\n");
-                break;
             case PO_BAD_USAGE:
                 print_bad_usage_message(rank);
                 break;
@@ -83,8 +73,6 @@ int main(int argc, char *argv[])
     }
 
     switch (po_ret) {
-        case PO_CUDA_NOT_AVAIL:
-        case PO_OPENACC_NOT_AVAIL:
         case PO_BAD_USAGE:
             MPI_CHECK(MPI_Finalize());
             exit(EXIT_FAILURE);
@@ -112,145 +100,57 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    {
+        // Print out the hosts to make sure that ranks are distributed as expected
+        int i;
+        for(i = 0; i < numprocs; i++) {
+            MPI_Barrier(MPI_COMM_WORLD);
+            if( rank == i ) {
+                char hname[1025];
+                gethostname(hname, 1024);
+                printf("rank %d: %s\n", rank, hname);
+            }
+        }
+        fflush(stdout);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
     if(rank == 0) {
         fprintf(stdout, HEADER);
         print_header(rank, BW);
-
-        if(options.window_varied) {
-            fprintf(stdout, "# [ pairs: %d ] [ window size: varied ]\n", options.pairs);
-            fprintf(stdout, "\n# Uni-directional Bandwidth (MB/sec)\n");
-        }
-
-        else {
-            fprintf(stdout, "# [ pairs: %d ] [ window size: %d ]\n", options.pairs,
-                    options.window_size);
-
-            if(options.print_rate) {
-                fprintf(stdout, "%-*s%*s%*s\n", 10, "# Size", FIELD_WIDTH,
-                        "MB/s", FIELD_WIDTH, "Messages/s");
-            }
-
-            else {
-                fprintf(stdout, "%-*s%*s\n", 10, "# Size", FIELD_WIDTH, "MB/s");
-            }
-        }
-
+        fprintf(stdout, "# [ pairs: %d ] [ window size: %d ]\n", options.pairs,
+                options.window_size);
+        fprintf(stdout, "%-*s%*s%*s\n", 10, "# Size", FIELD_WIDTH,
+                "MB/s", FIELD_WIDTH, "Messages/s");
         fflush(stdout);
     }
 
    /* More than one window size */
 
-   if(options.window_varied) {
-       int window_array[] = WINDOW_SIZES;
-       double ** bandwidth_results;
-       int log_val = 1, tmp_message_size = options.max_message_size;
-       int i, j;
+    /* Just one window size */
+    mbw_request = (MPI_Request *)malloc(sizeof(MPI_Request) * options.window_size);
+    mbw_reqstat = (MPI_Status *)malloc(sizeof(MPI_Status) * options.window_size);
 
-       for(i = 0; i < WINDOW_SIZES_COUNT; i++) {
-           if(window_array[i] > options.window_size) {
-               options.window_size = window_array[i];
-           }
-       }
+    for(curr_size = options.min_message_size; curr_size <= options.max_message_size; curr_size *= 2) {
+        double bw, rate;
 
-       mbw_request = (MPI_Request *) malloc(sizeof(MPI_Request) * options.window_size);
-       mbw_reqstat = (MPI_Status *) malloc(sizeof(MPI_Status) * options.window_size);
+        bw = calc_bw(rank, curr_size, numprocs - 1, options.window_size, s_buf, r_buf);
 
-       while(tmp_message_size >>= 1) {
-           log_val++;
-       }
+        if(rank == 0) {
+            rate = 1e6 * bw / curr_size;
 
-       bandwidth_results = (double **) malloc(sizeof(double *) * log_val);
-
-       for(i = 0; i < log_val; i++) {
-           bandwidth_results[i] = (double *)malloc(sizeof(double) *
-                   WINDOW_SIZES_COUNT);
-       }
-
-       if(rank == 0) {
-           fprintf(stdout, "#      ");
-
-           for(i = 0; i < WINDOW_SIZES_COUNT; i++) {
-               fprintf(stdout, "  %10d", window_array[i]);
-           }
-
-           fprintf(stdout, "\n");
-           fflush(stdout);
-       }
-    
-       for(j = 0, curr_size = options.min_message_size; curr_size <= options.max_message_size; curr_size *= 2, j++) {
-           if(rank == 0) {
-               fprintf(stdout, "%-7d", curr_size);
-           }
-
-           for(i = 0; i < WINDOW_SIZES_COUNT; i++) {
-               bandwidth_results[j][i] = calc_bw(rank, curr_size, options.pairs,
-                       window_array[i], s_buf, r_buf);
-
-               if(rank == 0) {
-                   fprintf(stdout, "  %10.*f", FLOAT_PRECISION,
-                           bandwidth_results[j][i]);
-               }
-           }
-
-           if(rank == 0) {
-               fprintf(stdout, "\n");
-               fflush(stdout);
-           }
-       }
-
-       if(rank == 0 && options.print_rate) {
-            fprintf(stdout, "\n# Message Rate Profile\n");
-            fprintf(stdout, "#      ");
-
-            for(i = 0; i < WINDOW_SIZES_COUNT; i++) {
-                fprintf(stdout, "  %10d", window_array[i]);
-            }       
-
-            fprintf(stdout, "\n");
-            fflush(stdout);
-
-            for(c = 0, curr_size = options.min_message_size; curr_size <= options.max_message_size; curr_size *= 2) {
-                fprintf(stdout, "%-7d", curr_size); 
-
-                for(i = 0; i < WINDOW_SIZES_COUNT; i++) {
-                    double rate = 1e6 * bandwidth_results[c][i] / curr_size;
-
-                    fprintf(stdout, "  %10.2f", rate);
-                }       
-
-                fprintf(stdout, "\n");
-                fflush(stdout);
-                c++;    
+            if(options.print_rate) {
+                fprintf(stdout, "%-*d%*.*f%*.*f\n", 10, curr_size,
+                        FIELD_WIDTH, FLOAT_PRECISION, bw, FIELD_WIDTH,
+                        FLOAT_PRECISION, rate);
             }
-       }
-   }
 
-   else {
-       /* Just one window size */
-       mbw_request = (MPI_Request *)malloc(sizeof(MPI_Request) * options.window_size);
-       mbw_reqstat = (MPI_Status *)malloc(sizeof(MPI_Status) * options.window_size);
-
-       for(curr_size = options.min_message_size; curr_size <= options.max_message_size; curr_size *= 2) {
-           double bw, rate;
-
-           bw = calc_bw(rank, curr_size, options.pairs, options.window_size, s_buf, r_buf);
-
-           if(rank == 0) {
-               rate = 1e6 * bw / curr_size;
-
-               if(options.print_rate) {
-                   fprintf(stdout, "%-*d%*.*f%*.*f\n", 10, curr_size,
-                           FIELD_WIDTH, FLOAT_PRECISION, bw, FIELD_WIDTH,
-                           FLOAT_PRECISION, rate);
-               }
-
-               else {
-                   fprintf(stdout, "%-*d%*.*f\n", 10, curr_size, FIELD_WIDTH,
-                           FLOAT_PRECISION, bw);
-               }
-           } 
-       }
-   }
+            else {
+                fprintf(stdout, "%-*d%*.*f\n", 10, curr_size, FIELD_WIDTH,
+                        FLOAT_PRECISION, bw);
+            }
+        }
+    }
 
    free_memory_pt2pt_mul(s_buf, r_buf, rank, options.pairs);
 
@@ -259,7 +159,7 @@ int main(int argc, char *argv[])
    return EXIT_SUCCESS;
 }
 
-double calc_bw(int rank, int size, int num_pairs, int window_size, char *s_buf,
+double calc_bw(int rank, int size, int writers, int window_size, char *s_buf,
         char *r_buf)
 {
     double t_start = 0, t_end = 0, t = 0, sum_time = 0, bw = 0;
@@ -270,8 +170,9 @@ double calc_bw(int rank, int size, int num_pairs, int window_size, char *s_buf,
 
     MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
 
-    if(rank < num_pairs) {
-        target = rank + num_pairs;
+    if(rank < writers) {
+        /* All processes are writing to a single one */
+        target = writers;
 
         for(i = 0; i <  options.iterations +  options.skip; i++) {
             if(i ==  options.skip) {
@@ -284,16 +185,11 @@ double calc_bw(int rank, int size, int num_pairs, int window_size, char *s_buf,
                         mbw_request + j));
             }
             MPI_CHECK(MPI_Waitall(window_size, mbw_request, mbw_reqstat));
-            MPI_CHECK(MPI_Recv(r_buf, 4, MPI_CHAR, target, 101, MPI_COMM_WORLD,
-                    &mbw_reqstat[0]));
+            MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
         }
-
         t_end = MPI_Wtime();
         t = t_end - t_start;
-    }
-
-    else if(rank < num_pairs * 2) {
-        target = rank - num_pairs;
+    } else if(rank == writers) {
 
         for(i = 0; i <  options.iterations +  options.skip; i++) {
             if(i ==  options.skip) {
@@ -301,25 +197,21 @@ double calc_bw(int rank, int size, int num_pairs, int window_size, char *s_buf,
             }
 
             for(j = 0; j < window_size; j++) {
-                MPI_CHECK(MPI_Irecv(r_buf, size, MPI_CHAR, target, 100, MPI_COMM_WORLD,
+                MPI_CHECK(MPI_Irecv(r_buf, size, MPI_CHAR, MPI_ANY_SOURCE, 100, MPI_COMM_WORLD,
                         mbw_request + j));
             }
 
             MPI_CHECK(MPI_Waitall(window_size, mbw_request, mbw_reqstat));
-            MPI_CHECK(MPI_Send(s_buf, 4, MPI_CHAR, target, 101, MPI_COMM_WORLD));
+            MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
         }
-    }
-
-    else {
-        MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
     }
 
     MPI_CHECK(MPI_Reduce(&t, &sum_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD));
 
     if(rank == 0) {
-        double tmp = size / 1e6 * num_pairs ;
+        double tmp = size / 1e6 * writers ;
         
-        sum_time /= num_pairs;
+        sum_time /= writers;
         tmp = tmp *  options.iterations * window_size;
         bw = tmp / sum_time;
 
