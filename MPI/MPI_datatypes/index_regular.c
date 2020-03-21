ae0d4f8f0e8736c3ff91a3226257dcc06b8a2fd5
Artem Polyakov <artpol84@gmail.com>
2020-03-20 22:24:54 -0700

    Migrate index_regular to the new common code

--- MPI/MPI_datatypes/index_regular.c
+++ MPI/MPI_datatypes/index_regular.c
@@ -1,53 +1,48 @@
 #include <stdio.h>
 #include <mpi.h>
+#include "utils.h"
 
 #ifndef START_IDX
 #define START_IDX 16
 #endif
+
+message_desc_t scenario[] = {
+    { 1, 4, {2}, {4} }
+};
 
 int main(int argc, char **argv)
 {
     MPI_Datatype type;
     int rank;
     MPI_Request req;
-    char buf[32], sync[1];
-    int count = sizeof(buf)/4;
-    int blocklens[count];
-    int displs[count];
+    message_t *m = NULL;
+    char *recv_buf, sync[1];
     int i;
+
+    m = message_init(NULL, 0, 0, 0, scenario, sizeof(scenario)/sizeof(scenario[0]));
+    ALLOC(recv_buf, m->outlen);
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    char *base_addr = &buf[START_IDX];
-
-    for(i=0; i < count; i++) {
-        blocklens[i] = 2;
-        displs[i] = &buf[i * 4] - base_addr;
-    }
-
-    MPI_Type_indexed(count, blocklens, displs, MPI_CHAR, &type);
+    MPI_Type_indexed(m->nblocks, m->blens, m->displs, MPI_CHAR, &type);
     MPI_Type_commit(&type);
     
     if( rank == 0 ){
         MPI_Send(sync, 1, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-        for(i=0; i < sizeof(buf); i++){
-            buf[i] = i % ('Z' - 'A' + 1) + 'A';
-        }
-        MPI_Isend(base_addr, 1, type, 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(m->base_addr, 1, type, 1, 0, MPI_COMM_WORLD, &req);
         MPI_Wait(&req, MPI_STATUS_IGNORE);
     } else {
         int i;
         MPI_Recv(sync, 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        for(i=0; i < sizeof(buf); i++){
-            buf[i] = 'x';
-        }
-
-        MPI_Recv(buf, 2 * count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(recv_buf, m->outlen, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Received: ");
-        for(i=0; i < count * 2; i++){
-            printf("%c ", buf[i]);
+        for(i=0; i < m->outlen; i++){
+            if( recv_buf[i] != m->outbuf[i] ){
+                printf("Message mismatch in offset=%d, expect '%c', got '%c'\n",
+                        i, m->outbuf[i], recv_buf[i]);
+            }
+            printf("%c ", recv_buf[i]);
         }
         printf("\n");
     }
