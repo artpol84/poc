46bf5cb38318f3229483070f92558ee1a02a26c9
Artem Polyakov <artpol84@gmail.com>
2020-03-22 10:49:32 -0700

    Add the new perf test (verify for now)

--- MPI/MPI_datatypes/index_perf.c
+++ MPI/MPI_datatypes/index_perf.c
@@ -5,4 +5,12 @@
 
 int main(int argc, char **argv)
 {
+    message_desc_t scenario[] = {
+        { 1, 1, { 30 }, {256} },
+        { 1, 23, {30*8}, {60*8} },
+
+    };
+    MPI_Init(&argc, &argv);
+    test_mpi_index(NULL, 0, 0, 0, scenario, sizeof(scenario)/sizeof(scenario[0]));
+    MPI_Finalize();
 }
