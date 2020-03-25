c19e404918380d63504e3af89b706f36995fba24
Artem Polyakov <artemp@mellanox.com>
2020-03-25 15:24:30 -0700

    Test zcopy/multi

---
+++ MPI/MPI_datatypes/index_zcopy_multi.c
@@ -0,0 +1,51 @@
+/*
+Copyright 2020 Artem Y. Polyakov <artpol84@gmail.com>
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may
+be used to endorse or promote products derived from this software without specific
+prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <mpi.h>
+#include "utils.h"
+
+#ifndef SEGM_SIZE
+#define SEGM_SIZE (8 * 1024)
+#endif
+
+#define REPCOUNT (128)
+
+int main(int argc, char **argv)
+{
+    int stride = SEGM_SIZE/REPCOUNT;
+    int payload = stride / 2;
+    message_desc_t scenario[] = {
+        { 1, REPCOUNT, {payload}, {stride} },
+        { 1, REPCOUNT, {payload}, {stride} },
+        { 1, REPCOUNT, {payload}, {stride} },
+        { 1, REPCOUNT, {payload}, {stride} }
+    };
+
+    MPI_Init(&argc, &argv);
+    test_mpi_index(NULL, BASE_RANGE, BASE_BUF, BASE_IDX,
+                   scenario, sizeof(scenario)/sizeof(scenario[0]), 2,
+                   RECV_TYPE, FORCE_UNEXP);
+    MPI_Finalize();
+}
