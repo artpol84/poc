380725d27396aa6bffbb9e02c75017957968f93d
Artem Y. Polyakov <artpol84@gmail.com>
2020-04-30 11:48:20 -0700

    Add vector test with 'count' != 1

--- MPI/MPI_datatypes/launch.sh
+++ MPI/MPI_datatypes/launch.sh
@@ -41,7 +41,7 @@ function exec_test()
     done
 }
 
-tests="vector vector_rcache index_plain index_regular_str index_regular_ilv index_ilv_w_block index_2x_ilv index_2x_str index_mixed1 index_two_dts index_zcopy_multi index_repro_bug1"
+tests="vector vector_cnt vector_rcache index_plain index_regular_str index_regular_ilv index_ilv_w_block index_2x_ilv index_2x_str index_mixed1 index_two_dts index_zcopy_multi index_repro_bug1"
 #tests="vector 9index_plain index_regular_str index_regular_ilv index_ilv_w_block index_2x_ilv index_2x_str index_mixed1 index_two_dts index_zcopy_multi"
 
 for t in $tests; do
---
+++ MPI/MPI_datatypes/vector_cnt.c
@@ -0,0 +1,72 @@
+/*
+Copyright 2020 Artem Y. Polyakov <artpol84@gmail.com>
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may
+be used to endorse or promote products derived from this software without specific
+prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <mpi.h>
+
+#define NCOUNT  2
+#define NBLOCKS 512
+#define BSIZE 2
+#define SSIZE 4
+#define SBUFSIZE (NBLOCKS * SSIZE * NCOUNT)
+#define RBUFSIZE (NBLOCKS * BSIZE * NCOUNT)
+
+int main(int argc, char **argv)
+{
+    int buf[SBUFSIZE];
+    MPI_Datatype type;
+    int rank;
+    MPI_Request req;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Type_vector((NBLOCKS), BSIZE, SSIZE, MPI_INT, &type);
+    MPI_Type_commit(&type);
+    
+    if( rank == 0 ){
+        int i;
+
+        MPI_Send(buf, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+
+        for(i=0; i < SBUFSIZE; i++){
+            buf[i] = i+1;
+        }
+        MPI_Isend(buf, NCOUNT, type, 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
+    } else {
+        int i;
+        MPI_Recv(buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(buf,RBUFSIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Received: ");
+        for(i=0; i<RBUFSIZE; i++){
+            printf("%d ", buf[i]);
+        }
+        printf("\n");
+    }
+
+    MPI_Type_free(&type);
+    
+    MPI_Finalize();
+}
