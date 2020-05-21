86c6bcd0fe2b5b391e593ac5a31887cc0e5302dc
Alexandr5Efimov <62000593+Alexandr5Efimov@users.noreply.github.com>
2020-05-21 18:32:08 +0700

    Update 1nmsc.h

--- arch/concurrency/locking/shmem_locking/1nmsc.h
+++ arch/concurrency/locking/shmem_locking/1nmsc.h
@@ -9,7 +9,8 @@ typedef struct msc_list_s {
 
 typedef struct msc_lock_s {
     msc_list_t *head;
-   msc_list_t cli_record;
+    msc_list_t cli_record;
+    msc_list_t srv_record;
 } msc_lock_t;
 
 /*
@@ -17,17 +18,17 @@ typedef struct  {
     pthread_mutex_t lock;
     volatile int signal;
 } nlock_elem_int_t;
-
-typedef struct  {
-    nlock_elem_int_t e;
-    char padding[64 - sizeof(nlock_elem_int_t)];
-} nlock_int_t;
 */
+typedef struct  {
+    msc_lock_t lock;
+    char padding[64 - sizeof(msc_lock_t)];
+} align_lock_t;
+
 
 typedef struct {
-    msc_lock_t locks[512];
-    /*cache line alignment */
-    msc_list_t srv_records[512];
+    align_lock_t locks[512];
+    /* cache line alignment */
+    /* msc_list_t srv_records[512]; */
 } nlock_t;
 
 #define my_lock_t nlock_t
