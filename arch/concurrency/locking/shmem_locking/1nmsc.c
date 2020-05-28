2c694d3eb2c830b7af0957800af6825c4b184704
Alexandr5Efimov <62000593+Alexandr5Efimov@users.noreply.github.com>
2020-05-28 07:51:18 +0700

    Update 1nmsc.c
    Bug fix

--- arch/concurrency/locking/shmem_locking/1nmsc.c
+++ arch/concurrency/locking/shmem_locking/1nmsc.c
@@ -13,10 +13,10 @@
 
 #define compiler_fence() { asm volatile ("" : : : "memory"); }
 
-//static int init_by_me = 0;
+static int init_by_me = 0;
 static int lock_num = 0;
 static int lock_idx = 0;
-static msc_list_t *my_records = NULL;
+static msc_list_t *my_record = NULL;
 
 int shared_rwlock_create(my_lock_t *lock) /* rank == 0 */
 {
@@ -31,7 +31,7 @@ int shared_rwlock_create(my_lock_t *lock) /* rank == 0 */
     for(i = 0; i < lock_num; i++) {
         lock->locks[i].e.head = NULL;
         lock->locks[i].e.cli_record.next = NULL;
-        lock->locks[i].e.svr_record.next = NULL;
+        lock->locks[i].e.srv_record.next = NULL;
     }
     
     MPI_Comm_rank(MPI_COMM_WORLD, &lock_idx);
@@ -62,7 +62,7 @@ inline void shared_rwlock_lock(msc_lock_t *msc_lock)
         // lock is taken, no one else is waiting
         return;
     }
-    my_record.locked = 1;
+    my_record->locked = 1;
     compiler_fence();
     prev->next = &my_record;
     compiler_fence();
@@ -71,7 +71,7 @@ inline void shared_rwlock_lock(msc_lock_t *msc_lock)
      * was unable to prevent compiler from optimizing this part of the
      * code resulting in a deadlock
      */
-    volatile uint32_t *flag = &my_record.locked;
+    volatile uint32_t *flag = &my_record->locked;
     while(*flag) {
         asm volatile (
             "label%=:\n"
@@ -87,7 +87,7 @@ inline void shared_rwlock_lock(msc_lock_t *msc_lock)
 
 void shared_rwlock_rlock(my_lock_t *lock)
 {
-    shared_rwlock_lock(lock->locks[lock_idx].e);
+    shared_rwlock_lock(&lock->locks[lock_idx].e);
 }
 
 void shared_rwlock_wlock(my_lock_t *lock)
@@ -95,18 +95,18 @@ void shared_rwlock_wlock(my_lock_t *lock)
     int i;
 
     for(i = 0; i < lock_num; i++) {
-        shared_rwlock_lock(lock->locks[i].e);
+        shared_rwlock_lock(&lock->locks[i].e);
     }
 }
 
-void shared_rwlock_ulock(msc_lock_t *lock)
+void shared_rwlock_ulock(msc_lock_t *msc_lock)
 {
     if (lock_idx != 0) // (!init_by_me)
         my_record = &msc_lock->cli_record;
     else 
         my_record = &msc_lock->srv_record;
     
-    if( CAS((int64_t*)&lock->head, (int64_t)&my_record, (int64_t)NULL) ) {
+    if( CAS((int64_t*)&msc_lock->head, (int64_t)&my_record, (int64_t)NULL) ) {
         // No one elase approached the lock after this thread
         return;
     }
@@ -119,11 +119,11 @@ void shared_rwlock_ulock(msc_lock_t *lock)
 
     // Release the next one in a queue
     compiler_fence();
-    my_record.next->locked =0;
+    my_record->next->locked =0;
     //asm volatile ("sfence" : : : "memory");
     // lock is released
-    my_record.next = NULL;
-    my_record.locked = 0;
+    my_record->next = NULL;
+    my_record->locked = 0;
 }
 
 void shared_rwlock_unlock(my_lock_t *lock)
@@ -131,10 +131,10 @@ void shared_rwlock_unlock(my_lock_t *lock)
     int i;
     if( init_by_me ){
         for(i = 0; i < lock_num; i++) {
-            shared_rwlock_ulock(lock->locks[i].e);
+            shared_rwlock_ulock(&lock->locks[i].e);
         }
     } else {
-        shared_rwlock_ulock(lock->locks[lock_idx].e);
+        shared_rwlock_ulock(&lock->locks[lock_idx].e);
     }
 }
 
