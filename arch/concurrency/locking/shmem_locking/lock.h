fc78137b4c95611374aaf9b492348a159d3d53b5
Alexandr V. Efimov <alexandr.v.efimov@gmail.com>
2020-07-13 17:42:02 +0700

    MSC locking scheme for shared memory RW was added.

--- arch/concurrency/locking/shmem_locking/lock.h
+++ arch/concurrency/locking/shmem_locking/lock.h
@@ -1,7 +1,7 @@
 #ifndef MY_LOCK_H
 #define MY_LOCK_H
 
-#if ((MY_FLOCK + MY_PTHREAD + MY_PTHREAD_MUTEX + MY_DUMMY + MY_C11_ATOMICS + MY_GCC_BUILDIN + MY_PTHREAD_N + MY_PTHREAD_N2 + MY_PTHREAD_N1 + MY_PTHREAD_N1S + MY_PTHREAD_NS1) != 1 )
+#if ((MY_FLOCK + MY_PTHREAD + MY_PTHREAD_MUTEX + MY_DUMMY + MY_C11_ATOMICS + MY_GCC_BUILDIN + MY_PTHREAD_N + MY_PTHREAD_N2 + MY_PTHREAD_N1 + MY_PTHREAD_N1S + MY_PTHREAD_NS1 + MY_MSC) != 1 )
 #error "Use one and only one locking type at the time"
 #endif
 
@@ -45,8 +45,8 @@
 #include "c11_atomics.h"
 #endif
 
-#if (MY_GCC_BUILDIN == 1)
-#include "build_in_atomics.h"
+#if (MY_MSC == 1)
+#include "1nmsc.h"
 #endif
 
 int shared_rwlock_create(my_lock_t *lock);
@@ -55,6 +55,5 @@ void shared_rwlock_wlock(my_lock_t *lock);
 void shared_rwlock_rlock(my_lock_t *lock);
 void shared_rwlock_unlock(my_lock_t *lock);
 int shared_rwlock_fin(my_lock_t *lock);
-
 
 #endif
