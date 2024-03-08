f6646d936c289a55b35705c4583db00a02761c15
Artem Y. Polyakov <artemp@nvidia.com>
2024-03-07 22:49:43 -0800

    Add sequential access measurement for comparison

--- arch/mem_sybsys/main.c
+++ arch/mem_sybsys/main.c
@@ -21,14 +21,14 @@ int main()
     /* Allocate data buffer */
     buf = calloc(cache_sizes[nlevels - 1], 1);
 
-    printf("Cache performance:\n");
+    printf("Cache performance (strided access):\n");
     for (i = 0; i < nlevels; i++)
     {
+        /* get 90% of the cache size */
         size_t wset = cache_sizes[i] - cache_sizes[i] / 10;
         uint64_t ts = rdtsc();
         for (j = 0; j < NACCESSES; j++)
         {
-            /* get 90% of the cache size */
 //            memset(buf, j, wset);
             size_t k,l;
             
@@ -44,12 +44,41 @@ int main()
             sum += buf[j];
         }
         
-        printf("[%d]:\twset=%zd, ts=%llu, %llu cyc/word, %llu cyc/byte, %llu B/sec\n",
+        printf("[%d]:\twset=%zd, ts=%llu, %llu cyc/word, %llu cyc/byte, %lf B/sec\n",
                 i, wset, ts, ts / (wset/8) / NACCESSES, ts / wset / NACCESSES,
                 (wset * NACCESSES)/(ts/clck_per_sec()));
     }
     
-    
+
+    printf("Cache performance (sequential access):\n");
+    for (i = 0; i < nlevels; i++)
+    {
+        /* get 90% of the cache size */
+        size_t wset = cache_sizes[i] - cache_sizes[i] / 10;
+        uint64_t ts = rdtsc();
+        for (j = 0; j < NACCESSES; j++)
+        {
+//            memset(buf, j, wset);
+            size_t k,l;
+            
+            for(k = 0; k < cl_size; k++){
+        	for(l = 0; l < wset / cl_size; l++){
+                    buf[l * cl_size + k] += 1;
+                }
+            }
+        }
+        ts = rdtsc() - ts;
+        
+        for(j = 0; j < wset; j++){
+            sum += buf[j];
+        }
+        
+        printf("[%d]:\twset=%zd, ts=%llu, %llu cyc/word, %llu cyc/byte, %lf B/sec\n",
+                i, wset, ts, ts / (wset/8) / NACCESSES, ts / wset / NACCESSES,
+                (wset * NACCESSES)/(ts/clck_per_sec()));
+    }
+ 
+
 
     return 0;
 }
