f963e9ace0853aab606c074f79b818a9f2d927af
Artem Y. Polyakov <artemp@nvidia.com>
2025-05-14 18:30:11 -0400

    Experiment with C++ const

---
+++ C++/const/const.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class internal {
+public:
+    int value;
+    internal() {
+        value = 0;
+    }
+
+    void inc() {
+        value++;
+    }
+};
+
+
+class outer {
+public:
+    internal *ptr;
+    outer() {
+        ptr = new internal;
+    }
+    
+    void test() const {
+        ptr->inc();
+    }
+};
+
+int main()
+{
+    outer t;
+    
+    t.test();
+}
